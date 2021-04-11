0d731f3e826d05f53143fce767933ab2a5fa9f5b
Natalia Antonina Dzieciol <naawiedzona.93@gmail.com>
2021-04-11 12:20:52 +0200

    falta button enviar

--- src/components/elementFormulari.vue
+++ src/components/elementFormulari.vue
@@ -4,45 +4,49 @@
         <input  :type="type" 
                 :placeholder="placeholder" 
                 :id="id" 
-                @input="$emit('input', $event.target.value)" 
-                @blur="CheckName"
-                :name="nameModel"
-                :v-model="v-model" >
-
-       <!--  
-           
-        <p :class="class"
-            :v-if="vIf"
-        >Required</p> 
-        
-        -->
-
-
-        
-        <!-- <p class="errorMessage" v-if="!$v.nameModel.required">Required</p>
-        <p class="errorMessage" v-if="!$v.nameModel.alpha">You can't use numbers</p>
-        <p class="errorMessage" v-else-if="(!$v.nameModel.minLength  || !$v.nameModel.maxLength)">Name should have between 6 and 13 letters</p> -->
-            
+                @blur="CheckInput"
+                v-model="valueInput" >
+        <p class="errorMessage" v-if="requireInput">Required</p> 
+        <p class="errorMessage" v-else-if="alphaInput">You can only use letters</p>
+        <p class="errorMessage" v-else-if="numberInput">You can only use numbers</p>
+        <p class="errorMessage" v-else-if="emailInput">You should put a real email adress</p>
+        <p class="errorMessage" v-else-if="passwordInput">The password sholud have between 6 and 13 letters, including capital letters and lower cases</p>
     </div>
 </template>
+
 <script>
-import { required, minLength, between, maxLength, email, alpha, sameAs } from 'vuelidate/lib/validators';
+//import { required, minLength, between, maxLength, email, alpha, sameAs } from 'vuelidate/lib/validators';
 
 export default {
+
+    data(){
+        return {
+            valueInput:"",
+            requireInput:false,
+            alphaInput:false,
+            numberInput:false,
+            emailInput:false,
+            passwordInput:false,
+            matchingPasswords:false
+        }
+    },
    props:[
        'label',
        'type',
        'placeholder',
        'id',
        'autofocus',
-       'v-model',
-       'name'
+       'require',
+       'alpha',
+       'numbers',
+       'emails',
+       'passwords'
   ],
-    validations: {
+    /* validations: {
     nameModel: {
         required,
-        minLength: minLength(6),
-        maxLength: maxLength(13),
+        //minLength: minLength(this.minLength),
+        //maxLength: maxLength(this.maxLength),
         alpha
     },
     phoneModel: {
@@ -67,12 +71,61 @@ export default {
         maxLength: maxLength(13),
         sameAsPassword:sameAs('passwordModel')
     }
-  },
+  }, */
   methods:{
-      CheckName(e){
-            return e;
-            //$v.phoneModel.$touch();
-            
+      CheckInput () {
+       //required
+        if (this.require) {
+            if(this.valueInput === "") {
+                this.requireInput = true;
+            }else{
+                this.requireInput = false;
+            }
+        // only letters
+            if(this.alpha) {
+                var letters = /^[A-Za-z]+$/;
+            if(!this.valueInput.match(letters)) {
+                this.alphaInput = true;
+            }else{
+                this.alphaInput = false;
+            }
+        }  
+        // only numbers 
+          if (this.numbers) {
+                var digits = /^[0-9]*$/;
+            if (!this.valueInput.match(digits)) {
+                this.numberInput = true;
+            }else {
+                this.numberInput = false;
+            }
+        } 
+        //email 
+          if (this.emails) {
+                var emails = /([a-zñ\d.-]+)@([a-zñ\d.-]+).([a-z]{2,})/gi;
+            if (!this.valueInput.match(emails)) {
+                this.emailInput = true;
+            }else {
+                this.emailInput = false;
+            }
+        } 
+        //password 
+         if (this.passwords) {
+                var passwords = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{6,13})$/;
+            if (!this.valueInput.match(passwords)) {
+                this.passwordInput = true;
+            }else {
+                this.passwordInput = false;
+            }
+        } 
+        //password 2  ??????????
+         /*  if(this.confirmPassword && this.password){
+            if(!this.valueInput!=) {
+                this.matchingPasswords = true;
+            }else{
+                this.matchingPasswords = false;
+            }
+        }  */
+        }  
       }
   }
 }
@@ -103,6 +156,11 @@ export default {
     font-family: monospace;
     font-size: 15px;
     color: rgb(76, 192, 173);
+}
+.errorMessage{
+    color: red;
+    font-size: 12px;
+    padding: -30px;
 }
 
 </style>
---
+++ src/components/submitButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="submitButton">
+         <input class="btn"  
+                :type="type"  
+                :value="value"
+                :id="id"
+                @click="SubmitForm">
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        SubmitForm () {
+            alert("submit form")
+        }
+    },
+    props: [
+        'type',
+        'id',
+        'value',
+        'sendForm'
+    ]
+}
+</script>
+<style scoped>
+
+.btn {
+    font-size: 20px;
+    border-radius: 4px;
+    padding: 8px;
+}
+#btn-send {
+    width: 60%;
+    margin-bottom: 30px;
+    background-color:rgb(76, 192, 173);
+    border: 1px solid rgb(76, 192, 173);
+    color: rgba(255, 255, 255, 0.685);
+}
+#btn-send:hover{
+    color: rgb(76, 192, 173);
+    background-color: rgba(255, 255, 255, 0.685);
+}
+</style>
